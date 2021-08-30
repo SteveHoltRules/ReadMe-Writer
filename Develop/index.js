cd524fce659488390a49aa041f33211f9dbf064c
Matt Luna <mat.c.luna@gmail.com>
2021-08-29 22:37:22 -0500

    Questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,35 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+// const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your README?',
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please name the readme');
+          return false;
+        }
+      },
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What type of license is included for this project?',
+      choices: ('MIT', 'Apache License 2.0', 'GNU GPLv2','GNU GPLv3','ISC License', 'The Unlicense') 
+    }
+  ]);
+};
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +38,20 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+// init()
+//   .then(writeToFile)
+
+questions;
+
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     return generatePage(portfolioData);
+//   })
+//   .then(pageHtml => {
+//     return writeFile(pageHtml);
+//   })
+//   .then(writeFileResponse => {
+//     console.log(writeFileResponse);
+//     return copyFile();
+//   })
