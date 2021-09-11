4919fdfa496906125df18e75c71f4c4b585eebf3
Matt Luna <mat.c.luna@gmail.com>
2021-09-11 10:24:01 -0500

    badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,40 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const { Octokit } = require("@octokit/core");
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
+
+const renderLicenseBadge() => {
+
+  fetch(Octokit.request('GET /licenses').then(response.json);
+
+  `https://shields.io/github/issues/SteveHoltRules/JestAnotherRPG`
+
+  //set username - repo data - from example walkthrough
+}
+
+console.log(renderLicenseBadge())
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
+// `;
+// }
+
+const genearteMarkdown = (title, description, toc, ul, ol, li, links, features, contributing, tests) => {
+  return `
+  #${title}
+  ${description}
+  ${}`
 }
 
 module.exports = generateMarkdown;
