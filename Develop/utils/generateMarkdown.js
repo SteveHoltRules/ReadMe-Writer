cb59cd2bf71a3cd994d02b26fb72c8fc4f62b648
Matt Luna <mat.c.luna@gmail.com>
2021-09-12 22:40:50 -0500

    index Json

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,40 +1,34 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 // TODO: Create a function that returns a license badge based on which license is passed in
-
 
 // If there is no license, return an empty string
 // function renderLicenseBadge(license) {}
 
 const renderLicenseBadge = (userName, repoName) => {
-//set username - repo data - from example walkthrough
-  `https://shields.io/github/issues/${userName}/${repoName}`
-} 
+  //set username - repo data - from example walkthrough
+  `https://shields.io/github/issues/${userName}/${repoName}`;
+};
 
-console.log(renderLicenseBadge())
+console.log(renderLicenseBadge());
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  `https://shields.io/github/${license}/${userName}/${repoName}`
-  //return the license link from the license json based on input
-  `https://shields.io/github/${license}/${userName}/${repoName}`
-
- }
+  `https://shields.io/github/${license}/${userName}/${repoName}`//return the license link from the license json based on input
+  `https://shields.io/github/${license}/${userName}/${repoName}`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 async function getPosts(userId) {
-  const response = await fetch(
-    `https://api.github.com/licenses/gpl-3.0`
-);
-
+  const response = await fetch(`https://api.github.com/licenses/gpl-3.0`);
+  if(!response.ok) {
+    throw 
+  }
 }
 
-
-
-const renderLicenseSection = function () { 
-
+const renderLicenseSection = function () {
   var apiUrl = `https://api.github.com/licenses/gpl-3.0`;
   console.log(apiUrl);
   fetch(apiUrl).then(function (response) {
@@ -44,20 +38,18 @@ const renderLicenseSection = function () {
         // var shortDesc = data.description;
         // console.log(shortDesc);
       });
-    };
-  })
+    }
+  });
   //parse the license and return it as parts
-
 };
 
 renderLicenseSection();
-  //
-  
-  // ${license
-  //     .filter(({ feature }) => feature)
-  //     .map(( { }))}
+//
 
- 
+// ${license
+//     .filter(({ feature }) => feature)
+//     .map(( { }))}
+
 // TODO: Create a function to generate markdown for README
 // function generateMarkdown(data) {
 //   return `# ${data.title}
